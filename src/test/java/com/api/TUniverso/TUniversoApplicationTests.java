701f875ff268f96db78aa2becbdba0e6ab5f9d02
Rafaflos <rfloressiaad@gmail.com>
2024-09-19 09:42:25 -0500

    ejemplo

---
+++ src/test/java/com/api/TUniverso/TUniversoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.TUniverso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TUniversoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
